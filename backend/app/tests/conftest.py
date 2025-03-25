dbdc534e51b1111d76e61e7a3d32a99812ebb48c
Fred Brunner <frdbrunner@gmail.com>
2025-03-25 12:53:54 +0100

    Fixed the issue with the tests

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,5 +1,9 @@
 import os
 import sys
+from pathlib import Path
 
 # Add the backend directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+# Add the parent directory to Python path
+sys.path.append(str(Path(__file__).parent.parent.parent))
